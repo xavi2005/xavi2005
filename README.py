afb2cb00e1eca26d68e60581a5ab2b2da96e58b4
xavi2005 <94165445+xavi2005@users.noreply.github.com>
2021-11-12 09:10:59 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @xavi2005
+- 👀 I’m interested in mathematical computation
+- 🌱 I’m currently learning how to create a systemal compututation of equacions
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+xavi2005/xavi2005 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
