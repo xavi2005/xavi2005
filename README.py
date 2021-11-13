a71b5c6ebf422f10fa18d7f2e3eb67dd287ab955
xavi2005 <94165445+xavi2005@users.noreply.github.com>
2021-11-13 17:36:05 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,18 @@
 - 👋 Hi, I’m @xavi2005
 - 👀 I’m interested in mathematical computation
-- 🌱 I’m currently learning how to create a systemal compututation of equacions
-- 💞️ I’m looking to collaborate on ...
+- 🌱 I’m currently learning 
+- 💞️ I’m looking to collaborate on enything(noob) 
 - 📫 How to reach me ...
 
 <!---
 xavi2005/xavi2005 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
+
+import pythhon
+from python imoport daytime 
+from daytime import time
+#Importo una serie de paquetes que se basan en el timpo para que pueda aprovecharlo...
+
+
+
